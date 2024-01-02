950441861b00b647bada78fb356e9a386c46e765
Jonathas Rodrigues <jonathashnr@gmail.com>
2024-01-02 19:01:20 -0300

    Adiciona funcao para criar rotas

--- cmd/ajudafortaleza/main.go
+++ cmd/ajudafortaleza/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 	"text/template"
 )
 
@@ -14,10 +16,10 @@ type appContext struct {
 
 func main() {
 	templates := template.Must(template.ParseGlob("templates/*.html"))
+	ctx := appContext{templates}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		templates.ExecuteTemplate(w, "main", nil)
-	})
+	mux.HandleFunc("/", ctx.homeHandler)
+	newRoute("GET /users/{id}", ctx.homeHandler)
 	addr := ":8080"
 	fmt.Println("Servidor escutando em http://localhost" + addr + "/")
 	log.Fatal(http.ListenAndServe(addr, mux))
@@ -25,4 +27,32 @@ func main() {
 
 func (c *appContext)homeHandler (w http.ResponseWriter, r *http.Request) {
 	c.templates.ExecuteTemplate(w, "main", nil)
+}
+
+type route struct {
+	method string
+	params []string
+	regex *regexp.Regexp
+	handler http.HandlerFunc
+}
+
+func newRoute(pattern string, handler http.HandlerFunc) route {
+	validadeRegex := regexp.MustCompile(`^(GET|POST|PUT|DELETE) (\/(?:[^\/\s]\/?)*)$`)
+	patternSubstrings := validadeRegex.FindStringSubmatch(pattern)
+	if len(patternSubstrings) != 3 {
+		panic("erro no processamento da rota: " + pattern)
+	}
+	method := patternSubstrings[1]
+	paramRegex := regexp.MustCompile(`{([^\/\s]+)}`)
+	paramSubstrings := paramRegex.FindAllStringSubmatch(patternSubstrings[2], -1)
+	var params []string
+	for _, paramSlice := range paramSubstrings {
+		params = append(params, paramSlice[1])
+	}
+	// Por padrão, esse roteador considerada paths terminados com ou sem "/"
+	// equivalentes, para mudar esse comportamento edite as duas linhas abaixo
+	replacedPath := strings.TrimRight(paramRegex.ReplaceAllString(patternSubstrings[2],`([^\/\s]+)`), "/")
+	pathRegex := regexp.MustCompile(fmt.Sprintf(`^%v/?$`, replacedPath))
+
+	return route{method, params, pathRegex, handler}
 }
