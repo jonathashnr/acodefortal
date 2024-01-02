9b336f32ec2ac5f1b2d6427b6caead643fddca52
Jonathas Rodrigues <jonathashnr@gmail.com>
2024-01-02 20:16:07 -0300

    Adiciona logica do router

--- cmd/ajudafortaleza/main.go
+++ cmd/ajudafortaleza/main.go
@@ -17,16 +17,23 @@ type appContext struct {
 func main() {
 	templates := template.Must(template.ParseGlob("templates/*.html"))
 	ctx := appContext{templates}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", ctx.homeHandler)
-	newRoute("GET /users/{id}", ctx.homeHandler)
+	router := router{[]route{
+		newRoute("GET /", ctx.homeHandler),
+		newRoute("GET /org/{id}", ctx.orgHandler),
+	}}
+	serve := http.HandlerFunc(router.serve)
 	addr := ":8080"
 	fmt.Println("Servidor escutando em http://localhost" + addr + "/")
-	log.Fatal(http.ListenAndServe(addr, mux))
+	log.Fatal(http.ListenAndServe(addr, serve))
 }
 
 func (c *appContext)homeHandler (w http.ResponseWriter, r *http.Request) {
 	c.templates.ExecuteTemplate(w, "main", nil)
+}
+
+func (c *appContext)orgHandler (w http.ResponseWriter, r *http.Request) {
+	id := PathValue(r, "id")
+	fmt.Fprintf(w, "O id da org é: %v", id)
 }
 
 type route struct {
@@ -55,4 +62,46 @@ func newRoute(pattern string, handler http.HandlerFunc) route {
 	pathRegex := regexp.MustCompile(fmt.Sprintf(`^%v/?$`, replacedPath))
 
 	return route{method, params, pathRegex, handler}
+}
+
+type router struct {
+	routes []route
+}
+type ctxkey struct {}
+
+func (router *router) serve(w http.ResponseWriter, r *http.Request) {
+	var allowedMethods []string
+	for _, route:= range router.routes {
+		matches := route.regex.FindStringSubmatch(r.URL.Path)
+		if len(matches) > 0 {
+			if r.Method != route.method {
+				allowedMethods = append(allowedMethods, route.method)
+				continue
+			}
+			paramValues := matches[1:]
+			// Não é para disparar nunca
+			if(len(route.params) != len(paramValues)) {
+				log.Printf("discrepância inesperada entre tamanho dos slices de params. route: %v. matches:%v", route.params, matches[1:])
+				continue
+			}
+			param := make(map[string]string)
+			for i, key := range route.params {
+				param[key] = paramValues[i]
+			}
+			context := context.WithValue(r.Context(), ctxkey{}, param)
+			route.handler(w,r.WithContext(context))
+			return
+		}
+	}
+	if len(allowedMethods) > 0 {
+		w.Header().Set("Allow", strings.Join(allowedMethods, ", "))
+		http.Error(w, "Método não suportado", http.StatusMethodNotAllowed)
+		return
+	}
+	http.Error(w, "Página não encontrada", http.StatusNotImplemented)
+}
+
+func PathValue(r *http.Request, key string) string {
+	param := r.Context().Value(ctxkey{}).(map[string]string)
+	return param[key]
 }
