49f631ae29202cf093bc269e71129a8b580ef0fd
Jonathas Rodrigues <jonathashnr@gmail.com>
2024-01-02 13:39:10 -0300

    Cria o servidor

--- cmd/ajudafortaleza/main.go
+++ cmd/ajudafortaleza/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Help Stronghold aka Ajuda Fortaleza :D")
+	})
+	addr := ":8080"
+	fmt.Println("Servidor escutando em http://localhost" + addr + "/")
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
