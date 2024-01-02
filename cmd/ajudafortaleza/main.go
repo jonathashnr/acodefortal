b12e2d8728973b233fe280c1543034f1ba7d5eaa
Jonathas Rodrigues <jonathashnr@gmail.com>
2024-01-02 14:17:54 -0300

    Adiciona parse de templates

--- cmd/ajudafortaleza/main.go
+++ cmd/ajudafortaleza/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
+type appContext struct {
+	templates *template.Template
+}
+
 func main() {
+	templates := template.Must(template.ParseGlob("templates/*.html"))
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Help Stronghold aka Ajuda Fortaleza :D")
+		templates.ExecuteTemplate(w, "main", nil)
 	})
 	addr := ":8080"
 	fmt.Println("Servidor escutando em http://localhost" + addr + "/")
 	log.Fatal(http.ListenAndServe(addr, mux))
+}
+
+func (c *appContext)homeHandler (w http.ResponseWriter, r *http.Request) {
+	c.templates.ExecuteTemplate(w, "main", nil)
 }
