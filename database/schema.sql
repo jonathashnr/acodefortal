8db0c25ef08a445c8cc2dd2d73023b33b2b9fd05
Jonathas Rodrigues <jonathashnr@gmail.com>
2024-01-09 17:54:47 -0300

    Expande schema

--- database/schema.sql
+++ database/schema.sql
@@ -20,4 +20,63 @@ CREATE TABLE sessao(
     expira INTEGER NOT NULL,
     FOREIGN KEY(usuario_id) REFERENCES usuario(id)
 );
+
+CREATE TABLE categoria(
+    id INTEGER PRIMARY KEY,
+    nome TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO categoria(nome) VALUES('Ajuda Humanitária');
+INSERT INTO categoria(nome) VALUES('Animais');
+INSERT INTO categoria(nome) VALUES('Cultura e Arte');
+INSERT INTO categoria(nome) VALUES('Desenvolvimento Comunitário');
+INSERT INTO categoria(nome) VALUES('Direitos Humanos');
+INSERT INTO categoria(nome) VALUES('Educação');
+INSERT INTO categoria(nome) VALUES('Meio Ambiente');
+INSERT INTO categoria(nome) VALUES('Saúde');
+
+CREATE TABLE org(
+    id INTEGER PRIMARY KEY,
+    nome TEXT NOT NULL,
+    slug TEXT NOT NULL UNIQUE,
+    categoria_id INTEGER NOT NULL,
+    missao TEXT NOT NULL,
+    criado INTEGER DEFAULT (unixepoch()) NOT NULL,
+    modificado INTEGER DEFAULT (unixepoch()) NOT NULL,
+    FOREIGN KEY(categoria_id) REFERENCES categoria(id)
+);
+
+CREATE TABLE endereco(
+    id INTEGER PRIMARY KEY,
+    org_id INTEGER NOT NULL UNIQUE,
+    logradouro TEXT NOT NULL,
+    numero TEXT NOT NULL,
+    bairro TEXT NOT NULL,
+    cidade TEXT NOT NULL,
+    uf TEXT NOT NULL,
+    cep TEXT NOT NULL,
+    complemento TEXT,
+    FOREIGN KEY(org_id) REFERENCES org(id)
+);
+
+CREATE TABLE contato(
+    id INTEGER PRIMARY KEY,
+    org_id INTEGER NOT NULL UNIQUE,
+    email TEXT,
+    telefone TEXT,
+    website TEXT,
+    instagram TEXT,
+    facebook TEXT,
+    twitter TEXT,
+    whatsapp TEXT,
+    FOREIGN KEY(org_id) REFERENCES org(id)
+);
+
+CREATE TABLE org_imagem(
+    id INTEGER PRIMARY KEY,
+    org_id INTEGER NOT NULL,
+    imagem BLOB NOT NULL,
+    FOREIGN KEY(org_id) REFERENCES org(id)
+);
+
 COMMIT;
