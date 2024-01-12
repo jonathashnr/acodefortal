220952dd488c590542181ba4b2751b1c54fcf53c
Jonathas Rodrigues <jonathashnr@gmail.com>
2024-01-12 18:46:58 -0300

    Começa a ajustar navbar/header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
     content: ["./templates/**/*.{html,js}"],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+            serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+            mono: [...defaultTheme.fontFamily.mono],
+        },
     },
     plugins: [require("@tailwindcss/forms")],
 };
